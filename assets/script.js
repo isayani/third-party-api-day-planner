2730a9f1a72814f54fa1bb0f8b95bf09018b0063
isayani <isayani95@gmail.com>
2022-08-04 00:03:45 -0400

    init commit with js steps mapped

---
+++ assets/script.js
@@ -0,0 +1,7 @@
+// Add in current date at top with moments js
+// color code time blocks (gray = past; red = current; green = future;)
+// add event listener on click to time block divs
+    // once clicked you can enter an event per block
+// event entered should save to local storage
+// when page is refreshed saved events persist
+
